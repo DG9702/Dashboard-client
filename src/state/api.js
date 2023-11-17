891e31a5ae348456c2a7af8ae2c4602c5d4fea62
Haibt9702 <buihaibq9702@gmail.com>
2023-11-17 17:36:46 +0700

    products page and customers page

--- src/state/api.js
+++ src/state/api.js
@@ -5,15 +5,27 @@ export const api = createApi({
     reducerPath: "adminApi",
     tagTypes: [
         "User",
+        "Products",
+        "Customers",
     ],
     endpoints: (build) => ({
         getUser: build.query({
             query: (id) => `general/user/${id}`,
             providesTags: ["User"],
         }),
+        getProducts: build.query({
+            query: () => "client/products",
+            providesTags: ["Products"],
+        }),
+        getCustomers: build.query({
+            query: () => "client/customers",
+            providesTags: ["Customers"],
+        }),
     }),
 });
 
 export const {
     useGetUserQuery,
+    useGetProductsQuery,
+    useGetCustomersQuery,
 } = api;
